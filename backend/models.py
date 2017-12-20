bd01677c8bbb01f85abfb681a5ec4494238f4fdc
Yukun Ma <gmayukun@gmail.com>
2017-12-20 19:03:26 +0800

    initialized database

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(primary_key=True, max_length=20)
+    password = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    friends = models.ManyToManyField("self")
+
+class Weibo(models.Model):
+    user = models.ForeignKey(User, related_name = "weibo", blank = True, null = False)
+    content = models.TextField(max_length=1000)
+    time = models.DateTimeField()
+
+class Comment(models.Model):
+    weibo = models.ForeignKey(Weibo, related_name="comments", blank=True, null=False)
+    user = models.ForeignKey(Weibo, related_name="comments", blank=True, null=False)
+    time = models.DateTimeField()
